45d8b8a1c5d31ec9ba02c8beb0525ba742ba9865
Maria Carolina <carolina.burgum@gmail.com>
2025-09-22 15:48:42 -0300

    Inclusão da função para adicionar os amigos na lista

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 // Criar a array que receb o nome dos amigos a serem sorteados
 let amigos = [];
+
+// Criação de função para adicionar os amigos que forem digitados,
+// enviando mensagem de erro caso nenhum nome seja digitado
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    // Valor deve ser diferente de vazio
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    // Adiciona à lista de amigos
+    amigos.push(nome);
+
+    // Limpa o campo para evitar errros
+    input.value = "";
+
+    // Incluir no próximo commit: Atualização da lista na tela
+    
+}

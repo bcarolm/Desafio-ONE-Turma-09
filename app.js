6d40c2b05cbd8be7c423d680e8b7043a52e2117d
Maria Carolina <carolina.burgum@gmail.com>
2025-09-22 15:55:40 -0300

    Inserção da funcionalidade de atualizar a lista de amigos e mostrá-la na tela

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ function adicionarAmigo() {
     // Limpa o campo para evitar errros
     input.value = "";
 
-    // Incluir no próximo commit: Atualização da lista na tela
+    //Atualização da lista na tela
+    atualizarLista();
     
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+
+    // Limpa a lista antes de renderizar novamente
+    lista.innerHTML = "";
+
+    // Percorre a lista e adiciona o nome do novo amigo, chamado de "li" para facilitar no HTML
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
